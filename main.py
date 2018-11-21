cd0ed8fc266a6907f1165298b00dcbae937e749f
IsraelAbebe <se.israel.abebe@gmail.com>
2018-11-21 11:41:29 +0300

    -- modefied main .py to work with additional inception models to be loaded later

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from keras.preprocessing import image
 import subprocess
 from keras.applications.inception_v3 import preprocess_input
 import argparse
+import sys
 
 parser = argparse.ArgumentParser()
 
 SPECIES = ['Apple','Bean','Blueberry','Cherry','Corn','Grape','Grapefruit','Orange','Peach','Pepper','Potato','Raspberry','Sorghum','Soybean','Squash','Strawberry','Sugarcane','Tomato']
-DISEASE_MODELS = ['Apple_0.9395_VGG.h5','Cherry_0.9873_VGG.h5','Corn_0.8926_VGG.h5','Grape_0.9293_VGG.h5','Peach_97_VGG.h5','Sugarcane_0.8356_VGG.h5',
-            'Tomato_0.8675_VGG.h5','pepper_95.90.h5','potato_90.62.h5','starwberry_99.h5']
+# DISEASE_MODELS = ['Apple_0.9395_VGG.h5','Cherry_0.9873_VGG.h5','Corn_0.8926_VGG.h5','Grape_0.9293_VGG.h5','Peach_97_VGG.h5','Sugarcane_0.8356_VGG.h5',
+#             'Tomato_0.8675_VGG.h5','pepper_95.90.h5','potato_90.62.h5','starwberry_99.h5']
 
 
 APPLE = ['Apple___Apple_scab','Apple___Black_rot','Apple___Cedar_apple_rust','Apple___healthy']
@@ -26,18 +27,21 @@ SUGERCANE = ['Sugarcane leaf spot', 'Sugarcane aphid', 'Sugarcane coal fouling']
 TOMATO = ['Tomato___Bacterial_spot','Tomato___Early_blight','Tomato___Late_blight','Tomato___Leaf_Mold','Tomato___Septoria_leaf_spot','Tomato___Spider_mites Two-spotted_spider_mite',
         'Tomato___Target_Spot','Tomato___Tomato_Yellow_Leaf_Curl_Virus','Tomato___Tomato_mosaic_virus','Tomato___healthy']
 
-SPECIOUS_MODEL ='VGG_all_100p_94.h5'
+
 
 target_size_disease = (64, 64)
 target_size_specious = (100, 100)
 
 
-def predict(img_path,do_print=True):
+def predict(img_path,do_print=True,model='VGG'):
+    """
+        given image path segment the image and predict specious on that image
+    
+    """
     image_name,extension=os.path.splitext(img_path)
     new_image = image_name+"_marked"+extension
-    print(new_image)
     result = subprocess.check_output(['python', "leaf-image-segmentation/segment.py", "-s", img_path])
-    model_path = os.path.join('Plant_Disease_Detection_Benchmark_models/Models', SPECIOUS_MODEL)
+    model_path = os.path.join('Plant_Disease_Detection_Benchmark_models/Models', get_species_model())
     model = load_model(model_path)
     img = Image.open(new_image)
     if img.size != target_size_specious:
@@ -57,8 +61,11 @@ def predict(img_path,do_print=True):
     return str(SPECIES[value[0]]),new_image
 
 
-def predict_species(img_path,do_print=True):
-    model_path = os.path.join('Plant_Disease_Detection_Benchmark_models/Models', SPECIOUS_MODEL)
+def predict_species(img_path,do_print=True,model='VGG'):
+    """
+        given the image predict the class on the raw image
+    """
+    model_path = os.path.join('Plant_Disease_Detection_Benchmark_models/Models', get_species_model())
     model = load_model(model_path)
     img = Image.open(img_path)
     if img.size != target_size_specious:
@@ -78,10 +85,13 @@ def predict_species(img_path,do_print=True):
     return str(SPECIES[value[0]])
     
 
-def predict_disease(img_path,species,do_print=True):
+def predict_disease(img_path,species,do_print=True,model='VGG'):
+    """
+       given image do disease prediction on that image 
+    """
     try:
         CLASS_ARRAY = get_class(species)
-        model_path = os.path.join('Plant_Disease_Detection_Benchmark_models/Models', get_model(species))
+        model_path = os.path.join('Plant_Disease_Detection_Benchmark_models/Models', get_disease_model(species))
     except:
         print ('NO Disease Found For This Species')
         return 0
@@ -105,19 +115,57 @@ def predict_disease(img_path,species,do_print=True):
     return str(CLASS_ARRAY[value[0]])
 
 
-def get_model(x):
-    return {
-        'Apple':'Apple_0.9395_VGG.h5',
-        'Cherry':'Cherry_0.9873_VGG.h5',
-        'Corn':'Corn_0.8926_VGG.h5',
-        'Grape':'Grape_0.9293_VGG.h5',
-        'Peach':'Peach_97_VGG.h5',
-        'Tomato':'Tomato_0.8675_VGG.h5',
-        'Pepper':'pepper_95.90.h5',
-        'Potato':'potato_90.62.h5',
-        'Strawberry':'starwberry_99.h5',
-        'Sugarcane':'Sugarcane_0.8356_VGG.h5'
-        }[x]
+def get_disease_model(x,model='VGG'):
+    '''
+         x : species name
+         model : which trained model VGG or Inception_V3
+    '''
+
+    if model == 'VGG':
+        return {
+            'Apple':'Apple_0.9395_VGG.h5',
+            'Cherry':'Cherry_0.9873_VGG.h5',
+            'Corn':'Corn_0.8926_VGG.h5',
+            'Grape':'Grape_0.9293_VGG.h5',
+            'Peach':'Peach_97_VGG.h5',
+            'Tomato':'Tomato_0.8675_VGG.h5',
+            'Pepper':'pepper_95.90.h5',
+            'Potato':'potato_90.62.h5',
+            'Strawberry':'starwberry_99.h5',
+            'Sugarcane':'Sugarcane_0.8356_VGG.h5'
+            }[x]
+    elif model == 'Inception_v3':
+        '''
+            TO BE CHANGED
+        '''
+        return {
+            'Apple':'Apple_0.9395_VGG.h5',
+            'Cherry':'Cherry_0.9873_VGG.h5',
+            'Corn':'Corn_0.8926_VGG.h5',
+            'Grape':'Grape_0.9293_VGG.h5',
+            'Peach':'Peach_97_VGG.h5',
+            'Tomato':'Tomato_0.8675_VGG.h5',
+            'Pepper':'pepper_95.90.h5',
+            'Potato':'potato_90.62.h5',
+            'Strawberry':'starwberry_99.h5',
+            'Sugarcane':'Sugarcane_0.8356_VGG.h5'
+            }[x]
+
+
+def get_species_model(model='VGG'):
+    '''
+         x : species name
+         model : which trained model VGG or Inception_V3
+    '''
+
+    if model == 'VGG':
+        return 'VGG_all_100p_94.h5'
+    elif model == 'Inception_v3':
+         '''
+            TO BE CHANGED
+        '''
+        return 'VGG_all_100p_94.h5'
+
 
 
 def get_class(x):
@@ -141,20 +189,37 @@ if __name__ == "__main__":
     parser.add_argument("--image",type=str,help='image path')
     parser.add_argument("--segment",type=bool,default=False,help='add segmentation')
     parser.add_argument("--species",type=str,default='',help='Specious Name if Known')
+    parser.add_argument('--model',default='VGG',choices=['VGG','Inception_v3'],help='choose  VGG or Inception_v3')
+
     args = parser.parse_args()
 
+    # Checking input and printing help 
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
+        
 
+
+    # if not segment and species is not known
     if False ==args.segment and args.species == '':
-        species = predict_species(args.image)
-        predict_disease(args.image,species)
+        species = predict_species(args.image,args.model)
+        predict_disease(args.image,species,args.model)
+
+    # if segment and species is not known     
     elif True == args.segment and args.species == '':
-        species,_ = predict(args.image)
+        species,_ = predict(args.image,args.model)
         predict_disease(args.image,species)
+
+    #if segment and species is given
     elif True == args.segment and args.species != '' :
-        species,image_name = predict(args.image,False)
-        predict_disease(image_name,species)
+        species,image_name = predict(args.image,False,args.model)
+        predict_disease(image_name,species,args.model)
+
+    #if not segment and species is given
     elif False == args.segment and args.species != '' :
-        predict_disease(args.image,species)
+        predict_disease(args.image,args.species,args.model)
+
+    # should not enter here
     else:
         Print("Make Sure Your Command is Correct")
 
